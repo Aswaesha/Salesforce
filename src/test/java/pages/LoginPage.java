8c6390524292e00b50d16dc24bbe9c8a7f725085
Aleksandra <taeyeononni@gmail.com>
2021-12-10 17:58:53 +0300

    Initial commit

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+
+public class LoginPage extends BasePage{
+
+    public static  final By USERNAME_INPUT = By.id("username");
+    public static  final By PASSWORD_INPUT = By.id("password");
+    public static  final By LOGIN_BUTTON = By.id("Login");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public boolean isPageOpen(){
+        return isExist(LOGIN_BUTTON);
+    }
+
+    public LoginPage open(){
+        driver.get(BASE_URL);
+        return this;
+
+    }
+
+    public HomePage login(String userName,String password){
+        driver.findElement(USERNAME_INPUT).sendKeys(userName);
+        driver.findElement(PASSWORD_INPUT).sendKeys(password);
+        driver.findElement(LOGIN_BUTTON).click();
+//        Actions action = new Actions(driver);
+//        action.click().sendKeys("").contextClick().clickAndHold().
+        return new HomePage(driver);
+    }
+}
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void login() {
+        boolean isHomePageOpened = loginPage
+                .open()
+                .login("taeyeononni-hegd@force.com", "Q1w2e3r4t5y6u7")
+                .isPageOpen();
+
+        assertTrue(isHomePageOpened, "Страница HomePage не открылась");
+
+    }
+
+
+}
